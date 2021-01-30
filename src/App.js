957c512cc2820b022dbd955d8c6aaf25e98711ab
Tamyla Ait-Chellouche <tamyla-emy@hotmail.fr>
2021-01-30 12:49:57 -0500

    supprimer learn

--- src/App.js
+++ src/App.js
@@ -9,17 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
         <h2>Allo {nom.toUpperCase()} !</h2>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
